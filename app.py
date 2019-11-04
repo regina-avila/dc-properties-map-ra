e23a08cef1846c1b8cf0705b2bf306dd2f751849
Avila <reginaatnist@gmail.com>
2019-11-04 11:14:30 -0500

    maria test

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import plotly.figure_factory as ff
 import pandas as pd
 
 # Read in the USA counties shape files
-from urllib.request import urlopen
-import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
+#from urllib.request import urlopen
+#import json
+# with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+#     counties = json.load(response)
 
 ########### Define a few variables ######
 
@@ -19,6 +19,7 @@ sourceurl = 'https://plot.ly/python/scattermapbox/'
 githublink = 'https://github.com/austinlasseter/dc-properties-map'
 mapbox_access_token = open("assets/mytoken.mapbox_token").read()
 df = pd.read_csv('resources/DC_Properties.csv', index_col='Unnamed: 0')
+#makes for a quicker run with fewer Properties
 df = df.sample(500)
 varlist=['BATHRM', 'HF_BATHRM', 'ROOMS', 'BEDRM', 'STORIES',  'PRICE']
 
@@ -29,25 +30,35 @@ server = app.server
 app.title=tabtitle
 
 ########## Figure
-fig = go.Figure(go.Scattermapbox(
+#this is a density mapbox. mapbox is a company that specializes in providing underlying maps for other startups to use in mapbuilding
+#plotly reached out for built-in mapbox functions. lots of go.mapbox functions available. will require lattitude and LONGITUDE
+#NOw turn this into a function
+def myfunc(some_value):
+    passig = go.Figure(go.Scattermapbox(
         lat=df['LATITUDE'],
         lon=df['LONGITUDE'],
         mode='markers',
         marker=go.scattermapbox.Marker(
             size=9,
-            colorscale='Reds',
-            color=df['PRICE']
+            colorscale='grays',
+            color=df[value]
         ),
         text=df['ASSESSMENT_SUBNBHD']
 
     ))
+return fig
+
 
 fig.update_layout(
     autosize=True,
     hovermode='closest',
     mapbox=go.layout.Mapbox(
+#must register for an access token on the mapbox website. this has to be saved. this one is in resources folder
+#mytoken.mapbox_access_token
+#gitignore - use this to ignore the filename with your token to hide it on github
         accesstoken=mapbox_access_token,
         bearing=0,
+#this centers on DC !
         center=go.layout.mapbox.Center(
             lat=38.92,
             lon=-77.07
@@ -89,7 +100,10 @@ app.layout = html.Div(children=[
 )
 
 ############ Callbacks
-
+@app.callback(Output('dc-map', 'figure'),
+             [Input('stats-drop', 'value')])
+def generate_map(dropdown_chosen_value):
+    return make_my_cool_figure(dropdown_chosen_value)
 
 
 ############ Deploy
